3a4a365b831c1055b8aaaf3a2db2146e1a2b824a
CarstenFrommhold <63000286+CarstenFrommhold@users.noreply.github.com>
2020-05-14 10:40:35 +0200

    added commentary

--- my_application/src/main.py
+++ my_application/src/main.py
@@ -1,8 +1,10 @@
+# FX Rates
 fx = {"US-Dollar": 1.20,
       "Kandaische Dollar": 1.52,
       "Schweizer Franken": 1.05,
       "Bitcoin": 0.00012}
 
+# Sell function
 def sell(euro_amount,currency):
     fx_rate = fx.get(currency)
     fx_amount = euro_amount * fx_rate
