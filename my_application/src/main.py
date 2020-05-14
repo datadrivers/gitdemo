3d9cb4f68f97d39db53c383afc6f452070635a3e
Carsten Frommhold <carsten.frommhold@datadrivers.de>
2020-05-14 10:31:03 +0200

    added bitcoin and changed usd

--- my_application/src/main.py
+++ my_application/src/main.py
@@ -1,6 +1,7 @@
-fx = {"US-Dollar": 1.08,
+fx = {"US-Dollar": 1.20,
       "Kandaische Dollar": 1.52,
-      "Schweizer Franken": 1.05}
+      "Schweizer Franken": 1.05,
+      "Bitcoin": 0.00012}
 
 def sell(euro_amount,currency):
     fx_rate = fx.get(currency)
